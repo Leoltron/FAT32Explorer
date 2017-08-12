aea2ff6927853dbbcd957d1f5183c90f883c9763
Leoltron <leoole5@gmail.com>
2017-08-12 21:56:03 +0500

    Fixed tests

--- tests.py
+++ tests.py
@@ -43,33 +43,33 @@ class BytesParserTests(unittest.TestCase):
         self.assertEqual("world!", parser.parse_string(7, 6, encoding=ASCII))
 
     def test_parse_time_start(self):  # 1:25:00
-        # [0000101100100000]
-        parser = BytesParser(b'\x0b\x20')
+        # [0010000000001011]
+        parser = BytesParser(b'\x20\x0b')
         self.assertEqual(datetime.time(hour=1, minute=25, second=0), parser.parse_time(0))
 
     def test_parse_time_middle(self):  # 1:25:00
-        # 00001111[0110000110000110]1010111100000000
-        parser = BytesParser(b'\x0F\x61\x86\xAF\x00')
+        # 00001111[1000011001100001]1010111100000000
+        parser = BytesParser(b'\x0F\x86\x61\xAF\x00')
         self.assertEqual(datetime.time(hour=12, minute=12, second=12), parser.parse_time(1))
 
     def test_parse_time_end(self):  # 17:35:54
-        # 0000101100101000[1000110001111011]
-        parser = BytesParser(b'\x0B\x28\x8C\x7B')
+        # 0000101100101000[0111101110001100]
+        parser = BytesParser(b'\x0B\x28\x7B\x8C')
         self.assertEqual(datetime.time(hour=17, minute=35, second=54), parser.parse_time(2))
 
     def test_parse_date_start(self):  # 09.08.2017(37) -> 37 08 09
-        # [0100101100001001]
-        parser = BytesParser(b'\x4B\x09\x00')
+        # [0000100101001011]00000000
+        parser = BytesParser(b'\x09\x4B\x00')
         self.assertEqual(datetime.date(year=2017, month=8, day=9), parser.parse_date(0))
 
     def test_parse_date_middle(self):  # 08.10.1998(18) -> 18 10 08
         # 110110000000101010000000[0010010101001000]10101011
-        parser = BytesParser(b'\xd8\x0a\x80\x25\x48\xab')
+        parser = BytesParser(b'\xd8\x0a\x80\x48\x25\xab')
         self.assertEqual(datetime.date(year=1998, month=10, day=8), parser.parse_date(3))
 
     def test_parse_date_end(self):  # 01.01.2000(20) -> 20 01 01
-        # 10111011110001101101010101010000010111[0010100000100001]
-        parser = BytesParser(b'\x2e\xf1\xb5\x54\x17\x28\x21')
+        # 10111011110001101101010101010000010111[0010000100101000]
+        parser = BytesParser(b'\x2e\xf1\xb5\x54\x17\x21\x28')
         self.assertEqual(datetime.date(year=2000, month=1, day=1), parser.parse_date(5))
 
 
