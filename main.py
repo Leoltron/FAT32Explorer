c01d3eaa8026f76987938f9640c288ad456d522e
Leoltron <leoole5@gmail.com>
2017-09-30 12:27:22 +0500

    Updated main.py a little.

--- main.py
+++ main.py
@@ -13,13 +13,18 @@ from pathlib import Path
 
 def main():
     image_file_name = ' '.join(sys.argv[1:])
+
+    if not image_file_name:
+        print("Usage: " + sys.argv[0] + " <file_name>")
+        return
+
     image_file_path = Path(image_file_name)
 
     if not image_file_path.exists():
-        print("File "+image_file_name + " not found.")
+        print('File "' + image_file_name + '" not found.')
         return
     if not image_file_path.is_file():
-        print(image_file_name + " is not a file.")
+        print('"' + image_file_name + '" is not a file.')
         return
 
     fi = open(image_file_name, "rb")
@@ -27,6 +32,7 @@ def main():
         f = fat_reader.Fat32Reader(fi.read())
     finally:
         fi.close()
+    print("Image successfully parsed.")
     d = f.get_root_directory()
     DirectoryBrowser(d).start_interactive_mode()
 
